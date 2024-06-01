b089c5c641acaa22e87757af23aab170edf174d6
josue rodriguez blanco <josuebrandon99@gmail.com>
2024-06-01 18:02:30 -0400

    pagina archivo

---
+++ src/app/archivo/page.tsx
@@ -0,0 +1,14 @@
+export default function Archivo(){
+    return(
+        <div className="container mx-auto">
+            <div className="h-screen ">
+            <p className="my-5 mt-[100px] text-4xl font-bold">PRÓXIMANTE</p>
+            <p className="text-xl"> Este sitio web se encuentra en  desarrollo, esta seccion aun no esta disponible...</p>
+            <hr />
+                
+    
+            </div>
+        </div>
+    )
+        
+}
